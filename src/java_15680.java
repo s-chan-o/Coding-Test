29e913d08025fb2f7ce48a9361c3bc79700914a6
김승찬 <s24041@gsm.hs.kr>
2024-08-02 14:58:28 +0900

    "BAEKJOON coding-test"

---
+++ src/java_15680.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class java_15680 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        if(N == 0){
+            System.out.println("YONSEI");
+        }else if(N == 1){
+            System.out.println("Leading the Way to the Future");
+        }
+    }
+}
