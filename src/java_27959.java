d436fd23ca592759188d2e4998ec5238314eb865
김승찬 <s24041@gsm.hs.kr>
2024-08-09 18:26:00 +0900

    "BAEKJOON coding-test"

---
+++ src/java_27959.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class java_27959 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+
+        if(100 * N >= M){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
